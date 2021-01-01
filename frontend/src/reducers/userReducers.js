32bb2e4bbccc26983e8465d912013d72f9b6660a
Marcos Vinicius <marcosvims@gmail.com>
2021-01-01 10:58:46 -0300

    shipping screen

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -10,6 +10,7 @@ import { USER_DETAILS_FAIL,
      USER_REGISTER_SUCCESS, 
      USER_UPDATE_PROFILE_FAIL, 
      USER_UPDATE_PROFILE_REQUEST,
+     USER_UPDATE_PROFILE_RESET,
      USER_UPDATE_PROFILE_SUCCESS} from "../constants/userConstants"
 
 export const userLoginReducer = (state = { }, action) => { 
@@ -61,6 +62,8 @@ export const userUpdateProfileReducer = (state = { }, action) => {
             return { loading: false, success: true, userInfo: action.payload }
         case USER_UPDATE_PROFILE_FAIL:
             return { loading: false, error: action.payload }
+        case USER_UPDATE_PROFILE_RESET:
+            return {}
         default:
             return state
     }
